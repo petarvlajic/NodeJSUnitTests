b6a46416543d046f77297cdb7292b76e8b44c201
Petar Vlajic <petar.vlajic@asioso.com>
2022-12-15 16:21:55 +0100

    Integration test

--- src/test/Authorization/Authorizer.test.ts
+++ src/test/Authorization/Authorizer.test.ts
@@ -1,60 +1,109 @@
 import { Authorizer } from "../../app/Authorization/Authorizer";
 import { SessionTokenDBAccess } from "../../app/Authorization/SessionTokenDBAccess";
 import { UserCredentialsDbAccess } from "../../app/Authorization/UserCredentialsDbAccess";
-import { Account, SessionToken } from "../../app/Models/ServerModels";
-jest.mock("../../app/Authorization/SessionTokenDBAccess");
-jest.mock("../../app/Authorization/UserCredentialsDbAccess");
+import { Account, SessionToken, TokenState } from "../../app/Models/ServerModels";
+jest.mock('../../app/Authorization/SessionTokenDBAccess');
+jest.mock('../../app/Authorization/UserCredentialsDbAccess');
 
-describe("Authorizer test suite", () => {
-  let authorizer: Authorizer;
 
-  const sessionTokenDBAccessMock = {
-    storeSessionToken: jest.fn(),
-  };
-  const userCredentialsDbAccessMock = {
-    getUserCredential: jest.fn(),
-  };
+const someAccount: Account = {
+    username: 'someUser',
+    password: 'somePassword'
+}
 
-  beforeEach(() => {
-    authorizer = new Authorizer(
-      sessionTokenDBAccessMock as any,
-      userCredentialsDbAccessMock as any
-    );
-  });
+describe('Authorizer test suite', () => {
+    let authorizer: Authorizer;
 
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
+    let sessionTokenDBAccessMock = {
+        storeSessionToken: jest.fn(),
+        getToken: jest.fn()
+    }
 
-  test("constructor arguments", () => {
-    new Authorizer();
-    expect(SessionTokenDBAccess).toBeCalled();
-    expect(UserCredentialsDbAccess).toBeCalled();
-  });
+    let userCredentialsDBAccessMock = {
+        getUserCredential: jest.fn()
+    }
 
-  const someAccount: Account = {
-    username: "someUser",
-    password: "password",
-  };
-
-  test("should return sessionToken for valid credentials", async () => {
-    jest.spyOn(global.Math, "random").mockReturnValueOnce(0);
-    jest.spyOn(global.Date, "now").mockReturnValueOnce(0);
-    userCredentialsDbAccessMock.getUserCredential.mockResolvedValueOnce({
-      username: "someUser",
-      accessRights: [1, 2, 3],
+    beforeEach(() => {
+        authorizer = new Authorizer(
+            sessionTokenDBAccessMock as any,
+            userCredentialsDBAccessMock as any
+        );
     });
-    const expectedSessionToken: SessionToken = {
-      userName: "someUser",
-      accessRights: [1, 2, 3],
-      valid: true,
-      tokenId: "",
-      expirationTime: new Date(60 * 60 * 1000),
-    };
-    const sessionToken = await authorizer.generateToken(someAccount);
-    expect(expectedSessionToken).toEqual(sessionToken);
-    expect(sessionTokenDBAccessMock.storeSessionToken).toBeCalledWith(
-      sessionToken
-    );
-  });
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+    test('constructor arguments', () => {
+        new Authorizer();
+        expect(SessionTokenDBAccess).toBeCalledTimes(1)
+        expect(UserCredentialsDbAccess).toBeCalledTimes(1)
+    })
+    describe('login user tests suite', () => {
+        test('should return null if invalid credentials', async () => {
+            userCredentialsDBAccessMock.getUserCredential.mockReturnValue(null);
+            const loginResult = await authorizer.generateToken(someAccount);
+            expect(loginResult).toBeNull;
+            expect(userCredentialsDBAccessMock.getUserCredential).
+                toBeCalledWith(someAccount.username, someAccount.password);
+        });
+
+        test('should return session token for valid credentials', async () => {
+            jest.spyOn(global.Math, 'random').mockReturnValueOnce(0);
+            jest.spyOn(global.Date, 'now').mockReturnValueOnce(0);
+            userCredentialsDBAccessMock.getUserCredential.mockReturnValue({
+                accessRights: [1, 2, 3],
+                username: 'someUser',
+                password: 'somePassword'
+            });
+            const expectedSessionToken: SessionToken = {
+                accessRights: [1, 2, 3],
+                userName: 'someUser',
+                valid: true,
+                expirationTime: new Date(1000 * 60 * 60),
+                tokenId: ''
+            }
+            const sessionToken = await authorizer.generateToken(someAccount);
+            expect(expectedSessionToken).toStrictEqual(sessionToken);
+            expect(sessionTokenDBAccessMock.storeSessionToken).toHaveBeenCalledWith(sessionToken);
+        });
+    });
+    describe('validateToken tests', () => {
+        test('validateToken returns invalid for null token', async () => {
+            sessionTokenDBAccessMock.getToken.mockReturnValueOnce(null);
+            const sessionToken = await authorizer.validateToken('123');
+            expect(sessionToken).toStrictEqual({
+                accessRights: [],
+                state: TokenState.INVALID
+            });
+        });
+
+        test('validateToken returns expired for expired tokens', async () => {
+            const dateInPast = new Date(Date.now() - 1);
+            sessionTokenDBAccessMock.getToken.mockReturnValueOnce({ valid: true, expirationTime: dateInPast });
+            const sessionToken = await authorizer.validateToken('123');
+            expect(sessionToken).toStrictEqual({
+                accessRights: [],
+                state: TokenState.EXPIRED
+            });
+        });
+
+        test('validateToken returns valid for not expired and valid tokens', async () => {
+            const dateInFuture = new Date(Date.now() + 100000);
+            sessionTokenDBAccessMock.getToken.mockReturnValue(
+                {
+                    valid: true,
+                    expirationTime: dateInFuture,
+                    accessRights: [1]
+                });
+            const sessionToken = await authorizer.validateToken('123');
+            expect(sessionToken).toStrictEqual({
+                accessRights: [1],
+                state: TokenState.VALID
+            });
+        });
+
+    });
+
+
+
+
 });
--- src/test/Utils/Utils.test.ts
+++ src/test/Utils/Utils.test.ts
@@ -1,31 +1,29 @@
-import { IncomingMessage } from "http";
-import { Utils } from "../../app/Utils/Utils";
+import { Utils } from '../../app/Utils/Utils'
+import { IncomingMessage } from 'http';
 
-describe("Utils test suite", () => {
-  test("getRequestPath valid request", () => {
-    const req = {
-      url: "http://localhost:8080/login",
-    } as IncomingMessage;
+describe('Utils test suite', () => {
 
-    const resultPath = Utils.getRequestBasePath(req);
-    expect(resultPath).toBe("login");
-  });
+    test('getRequestPath valid request', () => {
+        const request = {
+            url: 'http://localhost:8080/login'
+        } as IncomingMessage;
+        const resultPath = Utils.getRequestBasePath(request);
+        expect(resultPath).toBe('login');
+    });
 
-  test("getRequestPath with no path name", () => {
-    const req = {
-      url: "http://localhost:8080",
-    } as IncomingMessage;
+    test('getRequestPath with no path name', () => {
+        const request = {
+            url: 'http://localhost:8080/'
+        } as IncomingMessage;
+        const resultPath = Utils.getRequestBasePath(request);
+        expect(resultPath).toBeFalsy();
+    });
 
-    const resultPath = Utils.getRequestBasePath(req);
-    expect(resultPath).toBeFalsy();
-  });
-
-  test("getRequestPath with no url", () => {
-    const req = {
-      url: "",
-    } as IncomingMessage;
-
-    const resultPath = Utils.getRequestBasePath(req);
-    expect(resultPath).toBeFalsy();
-  });
+    test('getRequestPath with no path name', () => {
+        const request = {
+            url: ''
+        } as IncomingMessage;
+        const resultPath = Utils.getRequestBasePath(request);
+        expect(resultPath).toBeFalsy();
+    });
 });
