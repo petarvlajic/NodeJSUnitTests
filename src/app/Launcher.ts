149c9f791dab89a93afc66648c96ed6f741b3dce
Petar Vlajic <petar.vlajic@asioso.com>
2022-12-15 14:10:41 +0100

    Testing Server Launching

--- src/app/Launcher.ts
+++ src/app/Launcher.ts
@@ -1,16 +1,14 @@
-import { Server } from './Server/Server';
+import { Server } from "./Server/Server";
 
 export class Launcher {
-    
-    private server: Server;
+  private server: Server;
 
-    public constructor() {
-        this.server = new Server();
-    }
-    public launchApp() {
-        this.server.startServer();
-    }
-
+  public constructor() {
+    this.server = new Server();
+  }
+  public launchApp() {
+    this.server.startServer();
+  }
 }
 
 new Launcher().launchApp();
